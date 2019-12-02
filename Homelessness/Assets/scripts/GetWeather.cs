32a87a1cc75f33ac5cc2864d588319cddb25a599
Mohamed <only1hamada@live.com>
2019-12-02 10:41:52 -0500

    added script

--- Homelessness/Assets/scripts/GetWeather.cs
+++ Homelessness/Assets/scripts/GetWeather.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Networking;
 public class GetWeather : MonoBehaviour
 {
-    // Start is called before the first frame update
+    string key = "PUTYOURKEYHERE";
     void Start()
     {
-        
+        StartCoroutine(AskForWeather());
     }
-
-    // Update is called once per frame
-    void Update()
+    IEnumerator AskForWeather()
     {
-        
+        UnityWebRequest www = UnityWebRequest.Get("http://api.openweathermap.org/data/2.5/weather?zip=11238&mode=xml&APPID=" + key);
+        Debug.Log("Request sent, pending response...");
+        yield return www.SendWebRequest();
+        if (!www.isNetworkError && !www.isHttpError)
+        {
+            // Get text content like this:
+            Debug.Log(www.downloadHandler.text);
+        }
+        else
+        {
+            Debug.Log(www.error + " " + www);
+        }
     }
 }
