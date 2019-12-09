16eb525c391491b33ddbf28ff35b20aa0c958835
SilverMew22 <Shequanagarnett@gmail.com>
2019-12-09 11:47:56 -0500

    update

--- Homelessness/Assets/scripts/GetWeather.cs
+++ Homelessness/Assets/scripts/GetWeather.cs
@@ -6,18 +6,35 @@ public class GetWeather : MonoBehaviour
 {
     string key = "49fd35a8da2650013f629c4d0fce42f8";
     public string theData;
-
+    //public bool Contains(string theData, startIndex);
+    int startIndex = 0;
     void Start()
     {
         StartCoroutine(AskForWeather());
-            if (theData == "0") {
-            Debug.Log("yes");
-        } else {
-            Debug.Log("no");
+        //if (theData == "0")
+        //{
+        //    Debug.Log("yes");
+        //}
+        //else
+        //{
+        //    Debug.Log("no");
+        //}
+
+        if (theData.Contains("hello"))
+            Debug.Log("It says hello");
+        else
+            Debug.Log("It doesn't say hello");
+    }
+    void Update()
+    {
+        if (theData.Contains("rain"))
+        {
+            Debug.Log("rain");
         }
-     
-
-
+        else
+        {
+            Debug.Log("no rain");
+        }
     }
     IEnumerator AskForWeather()
     {
@@ -28,14 +45,12 @@ public class GetWeather : MonoBehaviour
         {
             // Get text content like this:
             theData = www.downloadHandler.text;
-            Debug.Log(www.downloadHandler.text);            
+            Debug.Log(www.downloadHandler.text);
         }
         else
         {
             Debug.Log(www.error + " " + www);
         }
-        
-        
     }
 }
 //http://api.openweathermap.org/data/2.5/weather?zip=11238&mode=xml&APPID=49fd35a8da2650013f629c4d0fce42f8
--- Homelessness/Assets/scripts/ex/WeatherController.cs
+++ Homelessness/Assets/scripts/ex/WeatherController.cs
@@ -6,92 +6,25 @@ using UnityEngine.Networking;
 
 public class WeatherController : MonoBehaviour
 {
-    private const string API_KEY = "49fd35a8da2650013f629c4d0fce42f8";
-    string key = "49fd35a8da2650013f629c4d0fce42f8";
-    private const float API_CHECK_MAXTIME = 10 * 60.0f; 
-    public string CityId;
-    public GameObject SnowSystem;
-    public GameObject RainSystem;
-    private float apiCheckCountdown = API_CHECK_MAXTIME;
+    //public string Substring(int startIndex);
 
+    private void Start()
+    {
 
-    [Serializable]
-    public class Weather
-    {
-        public int id;
-        public string main;
-    }
-    [Serializable]
-    public class WeatherInfo
-    {
-        public int id;
-        public string name;
-        public List<Weather> weather;
-    }  
-        
-    void Start()
-    {
-        StartCoroutine(GetWeather(CheckSnowStatus));
-        StartCoroutine(GetWeather(CheckRainStatus));
-        StartCoroutine(AskForWeather());
-    }
-    void Update()
-    {
-        apiCheckCountdown -= Time.deltaTime;
-        if (apiCheckCountdown <= 0)
-        {
-            apiCheckCountdown = API_CHECK_MAXTIME;
-            StartCoroutine(GetWeather(CheckSnowStatus));
-            StartCoroutine(GetWeather(CheckRainStatus));
-        }
-    }
+    
+    string[] info = { "Name: Felica Walker", "Title: Mz.",
+                   "Age: 47", "Location: Paris", "Gender: F"};
+    int found = 0;
 
-    public void CheckSnowStatus(WeatherInfo weatherObj)
-    {
-        bool snowing = weatherObj.weather[0].main.Equals("Snow");
-        if (snowing)
-            SnowSystem.SetActive(true);
-        else
-            SnowSystem.SetActive(false);
-    }
+    Console.WriteLine("The initial values in the array are:");
+foreach (string s in info)
+    Console.WriteLine(s);
 
-    public void CheckRainStatus(WeatherInfo weatherObj)
-    {
-        bool raining = weatherObj.weather[0].main.Equals("Rain");
-        if (raining)
-            RainSystem.SetActive(true);
-        else
-            RainSystem.SetActive(false);
-    }
-
-    IEnumerator GetWeather(Action<WeatherInfo> onSuccess)
-    {
-        using (UnityWebRequest req = UnityWebRequest.Get(String.Format("http://api.openweathermap.org/data/2.5/weather?zip=11238&mode=xml&APPID=", CityId, API_KEY)))
-        {
-            yield return req.Send();
-            while (!req.isDone)
-            yield return null;
-            byte[] result = req.downloadHandler.data;
-            string weatherJSON = System.Text.Encoding.Default.GetString(result);
-            WeatherInfo info = JsonUtility.FromJson<WeatherInfo>(weatherJSON);
-            onSuccess(info);
-        }
-    }
-    IEnumerator AskForWeather()
-    {
-        UnityWebRequest www = UnityWebRequest.Get("http://api.openweathermap.org/data/2.5/weather?zip=11238&mode=xml&APPID=" + key);
-        Debug.Log("Request sent, pending response...");
-        yield return www.SendWebRequest();
-        if (!www.isNetworkError && !www.isHttpError)
-        {
-            // Get text content like this:
-            Debug.Log(www.downloadHandler.text);
-
-        }
-        else
-        {
-            Debug.Log(www.error + " " + www);
-        }
-    }
+Console.WriteLine("\nWe want to retrieve only the key information. That is:");        
+foreach (string s in info) 
+{
+    found = s.IndexOf(": ");
+    Console.WriteLine("   {0}", s.Substring(found + 2));}
+}
 }
 
